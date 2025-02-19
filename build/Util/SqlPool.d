7e07f7f4d70a33c2209785a65585d7bde0d36532
StriverWo <iwq188166@gmail.com>
2025-02-19 16:24:15 +0800

    Add .gitignore file.

--- build/Util/SqlPool.d
+++ build/Util/SqlPool.d
@@ -1,2 +1,3 @@
 build/Util/SqlPool.o: src/Util/SqlPool.cpp /usr/include/stdc-predef.h
+
 /usr/include/stdc-predef.h:
--- build/Util/strptime_win.d
+++ build/Util/strptime_win.d
@@ -1,3 +1,4 @@
 build/Util/strptime_win.o: src/Util/strptime_win.cpp \
  /usr/include/stdc-predef.h
+
 /usr/include/stdc-predef.h:
