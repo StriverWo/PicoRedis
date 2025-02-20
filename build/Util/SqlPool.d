79649c1960f12f0c2ff6399e99bca2d9390fa9b1
StriverWo <132150106+StriverWo@users.noreply.github.com>
2025-02-20 13:46:43 +0800

    Delete build directory

--- build/Util/SqlPool.d
+++
@@ -1,3 +0,0 @@
-build/Util/SqlPool.o: src/Util/SqlPool.cpp /usr/include/stdc-predef.h
-
-/usr/include/stdc-predef.h:
--- build/Util/strptime_win.d
+++
@@ -1,4 +0,0 @@
-build/Util/strptime_win.o: src/Util/strptime_win.cpp \
- /usr/include/stdc-predef.h
-
-/usr/include/stdc-predef.h:
